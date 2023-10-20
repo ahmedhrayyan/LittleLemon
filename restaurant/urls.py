b2d75b41116b138a34c0a28ec49b7239cc35591f
Ahmed Hamed <ahmedhrayyan@outlook.com>
2023-10-20 20:34:23 +0300

    Add menu api

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path(r'', views.index, name='index'),
+    path(r'menu/', views.MenuItemView.as_view()),
+    path(r'menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
